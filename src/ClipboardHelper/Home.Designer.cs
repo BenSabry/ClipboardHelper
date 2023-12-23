32aa9865acc729378d20233e94b3831bc7a901c6
Ahmed Sabry <40901075+BenSabry@users.noreply.github.com>
2023-12-24 01:03:16 +0200

    Added Enable/Disable and Reset buttons

--- src/ClipboardHelper/Home.Designer.cs
+++ src/ClipboardHelper/Home.Designer.cs
@@ -28,17 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Home));
             richTextBox_Content = new RichTextBox();
             label_Message = new Label();
             linkLabel_github = new LinkLabel();
+            button_Enabled = new Button();
+            button_Reset = new Button();
             SuspendLayout();
             // 
             // richTextBox_Content
             // 
-            richTextBox_Content.Location = new Point(14, 16);
-            richTextBox_Content.Margin = new Padding(3, 4, 3, 4);
+            richTextBox_Content.Location = new Point(12, 12);
             richTextBox_Content.Name = "richTextBox_Content";
-            richTextBox_Content.Size = new Size(532, 127);
+            richTextBox_Content.Size = new Size(466, 96);
             richTextBox_Content.TabIndex = 0;
             richTextBox_Content.Text = "TEST Clipboard Helper No. {INCREMENT}/1000";
             richTextBox_Content.TextChanged += richTextBox_Content_TextChanged;
@@ -46,36 +48,62 @@
             // label_Message
             // 
             label_Message.AutoSize = true;
-            label_Message.Location = new Point(14, 148);
+            label_Message.Location = new Point(12, 111);
             label_Message.Name = "label_Message";
-            label_Message.Size = new Size(379, 20);
+            label_Message.Size = new Size(303, 15);
             label_Message.TabIndex = 1;
             label_Message.Text = "Press (KeysCombination) to paste the computed pattern";
             // 
             // linkLabel_github
             // 
             linkLabel_github.AutoSize = true;
-            linkLabel_github.Location = new Point(437, 148);
+            linkLabel_github.Location = new Point(12, 126);
             linkLabel_github.Name = "linkLabel_github";
-            linkLabel_github.Size = new Size(120, 20);
+            linkLabel_github.Size = new Size(96, 15);
             linkLabel_github.TabIndex = 2;
             linkLabel_github.TabStop = true;
             linkLabel_github.Text = "github/BenSabry";
             linkLabel_github.LinkClicked += linkLabel_github_LinkClicked;
             // 
+            // button_Enabled
+            // 
+            button_Enabled.Image = Properties.Resources.play;
+            button_Enabled.Location = new Point(398, 111);
+            button_Enabled.Name = "button_Enabled";
+            button_Enabled.Size = new Size(37, 34);
+            button_Enabled.TabIndex = 3;
+            button_Enabled.TextImageRelation = TextImageRelation.TextBeforeImage;
+            button_Enabled.UseVisualStyleBackColor = true;
+            button_Enabled.Click += button_Enabled_Click;
+            // 
+            // button_Reset
+            // 
+            button_Reset.Image = Properties.Resources.reset;
+            button_Reset.Location = new Point(441, 111);
+            button_Reset.Name = "button_Reset";
+            button_Reset.Size = new Size(37, 34);
+            button_Reset.TabIndex = 4;
+            button_Reset.TextImageRelation = TextImageRelation.TextBeforeImage;
+            button_Reset.UseVisualStyleBackColor = true;
+            button_Reset.Click += button_Reset_Click;
+            // 
             // Home
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(561, 177);
+            ClientSize = new Size(491, 150);
+            Controls.Add(button_Reset);
+            Controls.Add(button_Enabled);
             Controls.Add(linkLabel_github);
             Controls.Add(label_Message);
             Controls.Add(richTextBox_Content);
-            Margin = new Padding(3, 4, 3, 4);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Icon = (Icon)resources.GetObject("$this.Icon");
             MaximizeBox = false;
             Name = "Home";
             Text = "Clipboard Helper";
             FormClosing += Home_FormClosing;
+            Load += Home_Load;
             ResumeLayout(false);
             PerformLayout();
         }
@@ -85,5 +113,8 @@
         private RichTextBox richTextBox_Content;
         private Label label_Message;
         private LinkLabel linkLabel_github;
+        private Button button_Enabled;
+        private Button button1;
+        private Button button_Reset;
     }
 }
--- src/ClipboardHelper/Home.cs
+++ src/ClipboardHelper/Home.cs
@@ -1,82 +1,121 @@
 using System.Diagnostics;
 using System.Text;
 
-namespace ClipboardHelper
+namespace ClipboardHelper;
+
+public partial class Home : Form
 {
-    public partial class Home : Form
+    #region Properties
+    private GlobalKeyboardHook KeyboardHook;
+    private Keys[] KeysCombination = [Keys.F12];
+    private Size ButtonImageSize = new(20, 20);
+
+    private string WindowText = "";
+    private string EnabledStatus = "(Enabled)";
+    private string DisableStatus = "(Disabled)";
+
+    public bool IsEnabled { get; set; }
+    private Settings Settings { get; set; }
+    #endregion
+
+    #region Contructors
+    public Home()
     {
-        #region Properties
-        private GlobalKeyboardHook KeyboardHook;
-        private Keys[] KeysCombination = [Keys.F12];
+        InitializeComponent();
 
-        private Settings Settings { get; set; }
-        #endregion
-
-        #region Contructors
-        public Home()
-        {
-            InitializeComponent();
-
-            CustomLoad();
-        }
-        #endregion
-
-        #region Events
-        private void linkLabel_github_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
-        {
-            VisitGithub();
-        }
-        private void richTextBox_Content_TextChanged(object sender, EventArgs e)
-        {
-            UpdateContent();
-        }
-        private void Home_FormClosing(object sender, FormClosingEventArgs e)
-        {
-            KeyboardHook.Dispose();
-        }
-        private void KeyboardHook_KeyboardPressed(object? sender, GlobalKeyboardHookEventArgs e)
-        {
-            if (e.KeyboardState == GlobalKeyboardHook.KeyboardState.KeyDown)
-                HotKeyPressed();
-        }
-        #endregion
-
-        #region Behavior
-        private void CustomLoad()
-        {
-            UpdateDefaultMessage();
-            UpdateContent();
-            RegisterHotKey();
-        }
-        private void RegisterHotKey()
-        {
-            KeyboardHook = new GlobalKeyboardHook(KeysCombination);
-            KeyboardHook.KeyboardPressed += KeyboardHook_KeyboardPressed;
-        }
-        private void UpdateDefaultMessage()
-        {
-            label_Message.Text = label_Message.Text.Replace(nameof(KeysCombination),
-                string.Join(" + ", KeysCombination.Select(k => k.ToString())));
-        }
-        private void VisitGithub()
-        {
-            linkLabel_github.LinkVisited = true;
-            Process.Start("http://www.microsoft.com");
-        }
-        private void UpdateContent()
-        {
-            Settings = new Settings(richTextBox_Content.Text);
-        }
-        private void HotKeyPressed()
-        {
-            var clipboard = Clipboard.GetText();
-            Clipboard.SetText(Settings.ComputeContent());
-            SendKeys.Send(string.Empty);
-            SendKeys.SendWait("^{v}");
-            Clipboard.SetText(clipboard);
-        }
-        #endregion
+        OnInitialize();
     }
+    #endregion
+
+    #region Events
+    private void Home_Load(object sender, EventArgs e)
+    {
+        OnLoad();
+    }
+    private void linkLabel_github_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+    {
+        VisitGithub();
+    }
+    private void richTextBox_Content_TextChanged(object sender, EventArgs e)
+    {
+        UpdateContent();
+    }
+    private void Home_FormClosing(object sender, FormClosingEventArgs e)
+    {
+        KeyboardHook.Dispose();
+    }
+    private void KeyboardHook_KeyboardPressed(object? sender, GlobalKeyboardHookEventArgs e)
+    {
+        if (e.KeyboardState == GlobalKeyboardHook.KeyboardState.KeyDown)
+            HotKeyPressed();
+    }
+    private void button_Enabled_Click(object sender, EventArgs e)
+    {
+        UpdateIsEnabled();
+    }
+    private void button_Reset_Click(object sender, EventArgs e)
+    {
+        Settings.Reset();
+    }
+    #endregion
+
+    #region Behavior
+    private void OnInitialize()
+    {
+        UpdateContent();
+        RegisterHotKey();
+    }
+    private void OnLoad()
+    {
+        UpdateInfo();
+        UpdateIsEnabled();
+    }
+    private void RegisterHotKey()
+    {
+        KeyboardHook = new GlobalKeyboardHook(KeysCombination);
+        KeyboardHook.KeyboardPressed += KeyboardHook_KeyboardPressed;
+    }
+    private void UpdateInfo()
+    {
+        WindowText = this.Text;
+        button_Reset.Image = new Bitmap(Properties.Resources.reset, ButtonImageSize);
+
+        label_Message.Text = label_Message.Text.Replace(nameof(KeysCombination),
+            string.Join(" + ", KeysCombination.Select(k => k.ToString())));
+    }
+    private void VisitGithub()
+    {
+        linkLabel_github.LinkVisited = true;
+        Process.Start("http://www.microsoft.com");
+    }
+    private void UpdateContent()
+    {
+        Settings = new Settings(richTextBox_Content.Text);
+    }
+    private void UpdateIsEnabled()
+    {
+        IsEnabled = !IsEnabled;
+
+        this.Text = IsEnabled
+            ? $"{WindowText} {EnabledStatus}"
+            : $"{WindowText} {DisableStatus}";
+
+        richTextBox_Content.Enabled = IsEnabled;
+        button_Enabled.Image = IsEnabled
+            ? new Bitmap(Properties.Resources.pause, ButtonImageSize)
+            : new Bitmap(Properties.Resources.play, ButtonImageSize);
+    }
+    private void HotKeyPressed()
+    {
+        if (!IsEnabled) return;
+
+        var clipboard = Clipboard.GetText();
+        Clipboard.SetText(Settings.ComputeContent());
+        SendKeys.Send(string.Empty);
+        SendKeys.SendWait("^{v}");
+        Clipboard.SetText(clipboard);
+    }
+    #endregion
 }
 
 class Settings
@@ -88,13 +127,15 @@ class Settings
     {
         Content = content;
 
-        Pairs =
-        [
-            ("{INCREMENT}", "0", (s) => (int.Parse(s) + 1).ToString()),
-
-        ];
+        Reset();
     }
 
+    public void Reset()
+    {
+        Pairs = [
+            ("{INCREMENT}", "0", (s) => (int.Parse(s) + 1).ToString()),
+        ];
+    }
     public string ComputeContent()
     {
         var sb = new StringBuilder(Content);
